496e1ccf06336a2137f5ac64096fe6e16d24ae9c
chengdi <956591241@qq.com>
2021-03-27 20:41:43 +0800

    '规范保活'

--- resources/js/components/videoPlay.vue
+++ resources/js/components/videoPlay.vue
@@ -1,9 +1,7 @@
 <template>
   <div style="height: 100%;">
-    <video ref="myVideo1" autoplay controls width="100%" height="100%" src="" id="videoElement"></video>
-<!--    <video class="videoItem" :class="{'upup': isUp}" ref="myVideo1" autoplay controls width="100%" height="100%" src="" id="videoElement"></video>-->
-<!--    <video class="videoItem" :class="{'upup': !isUp}" ref="myVideo2" autoplay controls width="100%" height="100%" src="" id="vid"></video>-->
-    <div v-show="linkIsErr" class="errMessage">"{{link}}" - 当前链接无效</div>
+    <video ref="myVideo" autoplay controls width="100%" height="100%" src="" id="videoElement"></video>
+    <div v-show="linkIsErr" class="errMessage"> 当前链接无效 </div>
   </div>
 </template>
 
@@ -13,142 +11,74 @@ import axios from 'axios'
 export default {
   name: 'videoPlay',
   props: {
-    // src: 'http://1011.hlsplay.aodianyun.com/demo/game.flv',
     field: Object
   },
   data () {
     return {
-      flvPlayer: {
-        myVideo1: null,
-        myVideo2: null
-      },
+      flvPlayer: null,
       linkIsErr: false,
-      isUp: true,
-      aliveID: null,
-      deviceId: null,
-      liveCookie: null
+      aliveID: null, // 保活定时器ID
+      videoLink: null, // 视频播放链接
+      aliveVideoLink: null // 保活链接
     }
   },
   mounted () {
-    console.log('field: ', this.field)
-    this.deviceId = this.field.value
-    if(this.deviceId) {
-      this.gologin(this.field.deviceVideoLink, this.field.username, this.field.password)
-      // this.getVideos(this.deviceId, this.field.deviceVideoLink)
-      // this.createVideo(this.deviceId, 'myVideo1', true)
+    // console.log('field: ', this.field)
+    if(!this.field.deviceVideoLink) {
+      this.$toasted.show('缺少地址 —— deviceVideoLink !', { type: 'error' })
+      return
+    }
+    if(this.field.value && this.field.value.deviceID && this.field.value.channelID) {
+      console.log('开启保活')
+      this.aliveVideoLink = this.field.deviceVideoLink +
+        '/api/v1/devices/channelstream?device=' +
+        this.field.value.deviceID+'&channel=' +
+        this.field.value.channelID + '&protocol=FLV'
+      this.keepAlive(this.aliveVideoLink)
     } else {
       this.linkIsErr = true
     }
-    if (this.field.keepAlive && this.field.keepAlive.length) {
-      console.log('开启保活')
-      this.baohuo()
-    }
-    // this.qiehuan()
-    // setInterval(() => {
-    //   console.log('切换视频');
-    //   // this.isUp = !this.isUp
-    //   this.qiehuan()
-    // }, 45000)
   },
   methods: {
-    baohuo() {
-      this.qiehuan()
+    keepAlive(liveLink) {
+      this.getLiveVideos(liveLink)
       this.aliveID = setInterval(() => {
-        this.qiehuan()
+        this.getLiveVideos(liveLink)
       }, 45000);
     },
-    createVideo (link, videoName, isPlay) {
+    createVideo (link) {
       if (flvjs.isSupported()) {
-        const videoElement = this.$refs[videoName];
-        this.flvPlayer[videoName] = flvjs.createPlayer({
+        const videoElement = this.$refs.myVideo;
+        this.flvPlayer = flvjs.createPlayer({
           type: 'flv',
           isLive: true,
           hasAudio: false,
           hasVideo: true,
           url: link
         })
-        this.flvPlayer[videoName].attachMediaElement(videoElement)
-        this.flvPlayer[videoName].load()
-        if(isPlay) {
-          this.flvPlayer[videoName].play()
-          // console.log('播放 ', videoName);
-        }
+        this.flvPlayer.attachMediaElement(videoElement)
+        this.flvPlayer.load()
+        this.flvPlayer.play()
       }
     },
-    destruction(name) {
-      //销毁对象
-      name = this.isUp ? 'myVideo1' : 'myVideo2'
-      if(this.aliveID) clearInterval(this.aliveID)
-      if (this.flvPlayer[name]) {
-        // console.log('关闭 ', name);
-        // console.log('销毁视频对象');
-        this.flvPlayer[name].pause();
-        this.flvPlayer[name].destroy();
-        this.flvPlayer[name] = null;
+    destruction() {
+      if(this.aliveID) { // 关闭定时器
+        clearInterval(this.aliveID)
+        this.aliveID = null
+      }
+      if (this.flvPlayer) { // 销毁对象
+        this.flvPlayer.pause();
+        this.flvPlayer.destroy();
+        this.flvPlayer = null;
       }
     },
-    qiehuan() {
-      // console.log('是否 1 ', this.isUp);
-      // const dangqianname = this.isUp ? 'myVideo1' : 'myVideo2'
-      // const xianame = !this.isUp ? 'myVideo1' : 'myVideo2'
-      // this.flvPlayer[dangqianname].play()
-      // if(this.flvPlayer[xianame]) {
-      //   this.destruction(xianame)
-      // }
-      // setTimeout(() => {
-      //   this.gengxin(xianame)
-      // }, 30000)
-      this.gengxin('')
-    },
-    getAlive() {
-      axios.get(this.field.keepAlive)
-        .then(response => {
-          // console.log(response);
-        });
-    },
-    getVideos(deviceId, deviceVideoLink) {
-      axios.get(deviceVideoLink + '/api/v1/devices',{
-          // headers: {
-          //   "cookie" : this.liveCookie,
-          // },
-          transformRequest:[function (data1,headers){
-            console.log(headers.common)
-            delete headers.common['X-XSRF-TOKEN'];
-            return data1;
-          }]
-        }).then(response => {
-          console.log(response);
-        });
-    },
-    gologin(deviceVideoLink, username, password) {
-      axios.get(`${deviceVideoLink}/api/v1/login?username=${username}&password=${password}`, {
-            transformRequest:[function (data1,headers){
-              console.log(headers.common)
-              delete headers.common['X-XSRF-TOKEN'];
-              return data1;
-            }]
-          })
-          .then(response => {
-            console.log(response);
-            if(response.data.EasyDarwin && response.data.EasyDarwin.Body.Token) {
-              axios.defaults.withCredentials=true
-              this.liveCookie = `token=${response.data.EasyDarwin.Body.Token}`
-              this.getVideos(this.deviceId, this.field.deviceVideoLink)
-            }
-          });
-    },
-    gengxin(name) {
-      axios.get('https://live.ljfl.ltd:4443/api/v1/devices/channelstream?device=2&channel=2&protocol=FLV')
-        .then(response => {
-          // console.log(response);
-          // console.log('https://live.ljfl.ltd:4443')
-          // console.log(response.data.EasyDarwin.Body.URL);
-          // const url = 'https://live.ljfl.ltd:4443' + response.data.EasyDarwin.Body.URL
-          // console.log(url);
-          // console.log('加载 ', name);
-          // this.createVideo(url, name, false)
-          // this.qiehuan(url)
-          // this.createVideo(url, 'myVideo1', true)
+    getLiveVideos(liveLink) {
+      axios.get(liveLink).then(response => {
+          if(response.status != 200) return
+          if (!this.videoLink) { // 播放链接是空
+            this.videoLink = this.field.deviceVideoLink + response.data.EasyDarwin.Body.URL
+            this.createVideo(this.videoLink)
+          }
         });
     }
   }
@@ -169,13 +99,5 @@ video:focus {
   font-weight: 500;
   width: 100%;
   text-align: center;
-}
-.videoItem{
-  position: absolute;
-  left: 0;
-  z-index: 110;
-}
-.upup{
-  z-index: 120;
 }
 </style>
