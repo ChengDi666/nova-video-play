4f5580ba28ca0306190b6ebacd69369a0249d845
chengdi <956591241@qq.com>
2021-12-15 15:24:29 +0800

    '更新EasyPlayer依赖加载'

--- resources/js/field.js
+++ resources/js/field.js
@@ -3,7 +3,3 @@ Nova.booting((Vue, router, store) => {
   Vue.component('detail-video-play', require('./components/DetailField'))
   Vue.component('form-video-play', require('./components/FormField'))
 })
-const videoPlay = document.createElement('script');
-videoPlay.charset = 'utf-8';
-videoPlay.src = '/EasyPlayer-lib.min.js';
-document.head.appendChild(videoPlay);
